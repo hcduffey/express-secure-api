6228e88410e2078ff5eb1c22ad7033140fa47385
hcduffey <cliff.duffey@pm.me>
2022-06-06 15:37:51 -0400

    removed tmp files

--- tmp/hcduffey-SixtyThree-5bbbfca/controllers/index.js
+++
@@ -1,9 +0,0 @@
-// This is connecting the models, which connects to the db, into the controllers
-// require('../models')
-
-module.exports = {
-    ratings: require('./rating_controller'),
-    parks: require('./park_controller'),
-    users: require('./user_controller'),
-    auth: require('./auth_controller')
-}
